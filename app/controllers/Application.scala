612c66da664c0ef45238e28caddb63e086ace3bc
fahofah <fahriulucaycy@gmail.com>
2017-11-07 11:59:59 +0000

    Initialise

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,30 @@
 package controllers
 
-import play.api._
+
+import javax.inject.Inject
+
 import play.api.mvc._
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import reactivemongo.play.json.collection.JSONCollection
+//import models.{Feed, User}
+import play.api.libs.json.Json
+import reactivemongo.api.Cursor
+
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import reactivemongo.play.json._
+//import models.JsonFormats._
+import reactivemongo.api.commands.{UpdateWriteResult, WriteResult}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import reactivemongo.bson.BSONDocument
+
 
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("QA Games"))
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -1,15 +1,18 @@
-name := "QA_Games"
- 
-version := "1.0" 
-      
-lazy val `qa_games` = (project in file(".")).enablePlugins(PlayScala)
+name := "Play_Mongo"
+
+version := "1.0"
+
+lazy val `play_mongo` = (project in file(".")).enablePlugins(PlayScala)
 
 resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
-      
+
 scalaVersion := "2.11.11"
 
 libraryDependencies ++= Seq( jdbc , cache , ws , specs2 % Test )
 
-unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
+unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )
 
-      
+// only for Play 2.5.x
+libraryDependencies ++= Seq(
+  "org.reactivemongo" %% "play2-reactivemongo" % "0.12.6-play25"
+)
