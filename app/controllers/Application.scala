0bbb464e0b71b12dc09d67bb347134ce0368c53e
fahofah <fahriulucaycy@gmail.com>
2017-11-07 17:18:20 +0000

    end of Day1

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,6 +3,7 @@ package controllers
 
 import javax.inject.Inject
 
+import models.Game
 import play.api.mvc._
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 import reactivemongo.play.json.collection.JSONCollection
@@ -23,9 +24,13 @@ import reactivemongo.bson.BSONDocument
 
 class Application extends Controller {
 
-  val game1info = ("Game1","Party shooter", "+18", "£24.99")
-  val game2info = ("Game2","Role Play", "+18", "£44.99")
-  val basket = scala.collection.mutable.MutableList[String]()
+  var gamesList = scala.collection.mutable.Map[String, Game]()
+  val basket = scala.collection.mutable.MutableList[Game]()
+
+  val game1 =  Game("1","Overwatch","Party shooter", "+18", 24.99,"overwatch.jpg")
+  val game2 =  Game("2","Call of Duty","Role Play", "+18", 44.99,"CallofDuty.jpg")
+  gamesList += (game1._id -> game1)
+  gamesList += (game2._id -> game2)
 
   def index = Action {
     Ok(views.html.index("QA Games"))
@@ -33,12 +38,17 @@ class Application extends Controller {
 
   def displayGameInfo(game: String) = Action {
     game match {
-      case "game1" => Ok(views.html.gameScreen(game1info.toString,"images/overwatch.jpg"))
-      case "game2" => Ok(views.html.gameScreen(game2info.toString,"images/CallofDuty.jpg"))
+      case "game1" => Ok(views.html.gameScreen(game1))
+      case "game2" => Ok(views.html.gameScreen(game2))
     }
   }
 
-  def shoppingCart() = Action {
-    Ok("Added to basket!")
+  def calPrice(sum: Double, price: Double): Double ={
+    sum + price
+  }
+  def shoppingCart(gameId: String) = Action {
+    basket += gamesList(gameId)
+    val theGame = gamesList(gameId)
+    Ok(s"${theGame.name} added to basket!\nYour cart now: ${basket.map(_.toString)}\nTotal Price: ${basket.foldLeft(0.0)((sum, game) => calPrice(sum,game.price))}")
   }
 }
---
+++ app/models/Game.scala
@@ -0,0 +1,5 @@
+package models
+
+case class Game (_id: String,name: String, categoty: String, ageRating: String, price: Double, image: String) {
+
+}
