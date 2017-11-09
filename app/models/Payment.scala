38eb8d6e27aff813e3fa67c284ccd8969118b63e
fahofah <fahriulucaycy@gmail.com>
2017-11-09 09:44:44 +0000

    Day3 pre-mongo

--- app/models/Payment.scala
+++ app/models/Payment.scala
@@ -1,19 +1,22 @@
 package models
 import java.util.Date
+
+import org.joda.time.DateTime
 import play.api.data._
 import play.api.data.Forms._
 
-case class Payment(name: String, number: String, expiry: Date, csv: Int) {
+case class Payment(name: String, number: String, expiry: DateTime, csv: Int) {
 
 }
 
 object Payment {
 
+  val endDate = new Date("12/17")
   val createForm = Form(
     mapping (
       "name" -> nonEmptyText,
       "number" -> nonEmptyText(minLength = 16, maxLength = 16),
-      "expiry" -> date(pattern = "mm/yy"),
+      "expiry" -> jodaDate(pattern = "mm/yy"),
       "csv" -> number(min = 100, max = 999)
   )(Payment.apply)(Payment.unapply)
   )
